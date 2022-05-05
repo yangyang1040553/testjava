fb8167b1a0564717a6eae66fa7c32da07981daf8
xiaoxingkong <xingxing@qq.com>
2022-05-05 18:30:45 +0700

    init

--- hash-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ hash-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.ComponentScans;
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class RuoYiApplication
 {
+
     public static void main(String[] args)
     {
         // System.setProperty("spring.devtools.restart.enabled", "false");
