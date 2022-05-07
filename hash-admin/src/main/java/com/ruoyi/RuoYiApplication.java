f6c3279bb197d4c148afb9d73b9bda24c8bfc9eb
xiaoxingkong <xingxing@qq.com>
2022-05-07 11:04:27 +0700

    更新依赖配置

--- hash-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ hash-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -19,7 +19,7 @@ public class RuoYiApplication
     {
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(RuoYiApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  后题服务启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+        System.out.println("(♥◠‿◠)ﾉﾞ  后台服务启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
                 " .-------.       ____     __        \n" +
                 " |  _ _   \\      \\   \\   /  /    \n" +
                 " | ( ' )  |       \\  _. /  '       \n" +
