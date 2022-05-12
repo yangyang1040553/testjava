0a2a373b1decad4d3213b86ed0672cad67cc19a6
xiaoxingkong <xingxing@qq.com>
2022-05-12 13:55:02 +0700

    redis缓存

---
+++ hash-game/src/main/java/com/ruoyi/game/redis/GameRedis.java
@@ -0,0 +1,23 @@
+package com.ruoyi.game.redis;
+
+
+import com.ruoyi.common.core.redis.RedisCache;
+import com.ruoyi.common.redis.RedisKey;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GameRedis {
+
+    @Autowired
+    RedisCache redisCache;
+
+
+    /***
+     * 删除游戏配置缓存
+     * @param id
+     */
+    public void deleteGameConfig(String id) {
+        redisCache.delCacheMapValue(RedisKey.game_config, id);
+    }
+}
