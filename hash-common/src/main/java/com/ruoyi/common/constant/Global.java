b6ec2fad24816f79b314e9a23cf34b66f04cebd6
xiaoxingkong <xingxing@qq.com>
2022-05-12 16:10:18 +0700

    公共方法

---
+++ hash-common/src/main/java/com/ruoyi/common/constant/Global.java
@@ -0,0 +1,58 @@
+package com.ruoyi.common.constant;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+/***
+ * 全局配置
+ */
+public class Global {
+
+    public final static String USER_SECRET_KEY = "HASH-GAME_2022_18888888888888";
+
+
+
+    /***
+     * 获取玩家id
+     */
+    public static String getUserId(HttpServletRequest request) {
+        final Object userId = request.getAttribute("userId");
+        if (userId != null) {
+            return userId.toString();
+        }
+        return null;
+    }
+
+
+
+    /***
+     * 获取币种转换后的金额
+     */
+    public static long getWalletAmount(long amount) {
+        return amount * 10000;
+    }
+
+    /**
+     * 得到钱包余额的真实值
+     *
+     * @param amount
+     * @return
+     */
+    public static double getWalletAmountToReal(long amount) {
+        return amount / 10000;
+    }
+
+    /**
+     * 得到钱包余额的真实值
+     *
+     * @param amount
+     * @return
+     */
+    public static String getWalletAmountStrToReal(long amount) {
+        return String.valueOf(amount / 10000);
+    }
+
+
+
+
+}
