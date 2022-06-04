de80aa5babc22c5e60baff18d74bcf3fafaefeda
huilang <yangyang1040553@gmail.com>
2022-06-04 19:43:23 +0700

    修改用户 查询 修改钱包查询

--- hash-wallet/src/main/java/com/ruoyi/audit/domain/WalletPlayerData.java
+++ hash-wallet/src/main/java/com/ruoyi/audit/domain/WalletPlayerData.java
@@ -7,115 +7,130 @@ import com.ruoyi.common.core.domain.BaseEntity;
 
 /**
  * 用户钱包对象 t_wallet_player_data
- * 
+ *
  * @author xxk
  * @date 2022-05-08
  */
-public class WalletPlayerData extends BaseEntity
-{
+public class WalletPlayerData extends BaseEntity {
     private static final long serialVersionUID = 1L;
 
-    /** 唯一id（玩家id） */
+    /**
+     * 唯一id（玩家id）
+     */
     private String id;
 
-    /** usdt余额 */
+    /**
+     * usdt余额
+     */
     @Excel(name = "usdt余额")
     private Long usdtAmount;
 
-    /** trx余额 */
+    /**
+     * trx余额
+     */
     @Excel(name = "trx余额")
     private Long trxAmount;
 
-    /** Usdt的hash地址 */
+    /**
+     * Usdt的hash地址
+     */
     @Excel(name = "Usdt的hash地址")
     private String hashAddressUsdt;
 
-    /** Trx的hash地址 */
+    /**
+     * Trx的hash地址
+     */
     @Excel(name = "Trx的hash地址")
     private String hashAddressTrx;
 
-    /** 总充值金额 */
+    /**
+     * 总充值金额
+     */
     @Excel(name = "总充值金额")
     private Long rechargeTotal;
 
-    /** 总提现金额 */
+    /**
+     * 总提现金额
+     */
     @Excel(name = "总提现金额")
     private Long withdrawTotal;
 
-    public void setId(String id) 
-    {
+    private String invitationCode;
+
+    public String getInvitationCode() {
+        return invitationCode;
+    }
+
+    public void setInvitationCode(String invitationCode) {
+        this.invitationCode = invitationCode;
+    }
+
+    public void setId(String id) {
         this.id = id;
     }
 
-    public String getId() 
-    {
+    public String getId() {
         return id;
     }
-    public void setUsdtAmount(Long usdtAmount) 
-    {
+
+    public void setUsdtAmount(Long usdtAmount) {
         this.usdtAmount = usdtAmount;
     }
 
-    public Long getUsdtAmount() 
-    {
+    public Long getUsdtAmount() {
         return usdtAmount;
     }
-    public void setTrxAmount(Long trxAmount) 
-    {
+
+    public void setTrxAmount(Long trxAmount) {
         this.trxAmount = trxAmount;
     }
 
-    public Long getTrxAmount() 
-    {
+    public Long getTrxAmount() {
         return trxAmount;
     }
-    public void setHashAddressUsdt(String hashAddressUsdt) 
-    {
+
+    public void setHashAddressUsdt(String hashAddressUsdt) {
         this.hashAddressUsdt = hashAddressUsdt;
     }
 
-    public String getHashAddressUsdt() 
-    {
+    public String getHashAddressUsdt() {
         return hashAddressUsdt;
     }
-    public void setHashAddressTrx(String hashAddressTrx) 
-    {
+
+    public void setHashAddressTrx(String hashAddressTrx) {
         this.hashAddressTrx = hashAddressTrx;
     }
 
-    public String getHashAddressTrx() 
-    {
+    public String getHashAddressTrx() {
         return hashAddressTrx;
     }
-    public void setRechargeTotal(Long rechargeTotal) 
-    {
+
+    public void setRechargeTotal(Long rechargeTotal) {
         this.rechargeTotal = rechargeTotal;
     }
 
-    public Long getRechargeTotal() 
-    {
+    public Long getRechargeTotal() {
         return rechargeTotal;
     }
-    public void setWithdrawTotal(Long withdrawTotal) 
-    {
+
+    public void setWithdrawTotal(Long withdrawTotal) {
         this.withdrawTotal = withdrawTotal;
     }
 
-    public Long getWithdrawTotal() 
-    {
+    public Long getWithdrawTotal() {
         return withdrawTotal;
     }
 
     @Override
     public String toString() {
-        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-            .append("id", getId())
-            .append("usdtAmount", getUsdtAmount())
-            .append("trxAmount", getTrxAmount())
-            .append("hashAddressUsdt", getHashAddressUsdt())
-            .append("hashAddressTrx", getHashAddressTrx())
-            .append("rechargeTotal", getRechargeTotal())
-            .append("withdrawTotal", getWithdrawTotal())
-            .toString();
+        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+                .append("id", getId())
+                .append("usdtAmount", getUsdtAmount())
+                .append("trxAmount", getTrxAmount())
+                .append("hashAddressUsdt", getHashAddressUsdt())
+                .append("hashAddressTrx", getHashAddressTrx())
+                .append("rechargeTotal", getRechargeTotal())
+                .append("withdrawTotal", getWithdrawTotal())
+                .toString();
     }
 }
