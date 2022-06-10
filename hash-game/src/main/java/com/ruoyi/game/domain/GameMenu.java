2ac4d364eb264aa0922b465bd374361d7906e4a1
huilang <yangyang1040553@gmail.com>
2022-06-10 12:23:30 +0700

    修改 用户活动 修改 用户banner 修改 用户游戏菜单配置

--- hash-game/src/main/java/com/ruoyi/game/domain/GameMenu.java
+++ hash-game/src/main/java/com/ruoyi/game/domain/GameMenu.java
@@ -7,74 +7,99 @@ import com.ruoyi.common.core.domain.BaseEntity;
 
 /**
  * 游戏菜单对象 t_game_menu
- * 
+ *
  * @author xxk
  * @date 2022-05-12
  */
-public class GameMenu extends BaseEntity
-{
+public class GameMenu extends BaseEntity {
     private static final long serialVersionUID = 1L;
 
-    /** 唯一id */
+    /**
+     * 唯一id
+     */
     @Excel(name = "唯一id")
     private Long id;
 
-    /** 菜单名称 */
+    /**
+     * 菜单名称
+     */
     @Excel(name = "菜单名称")
     private String menuName;
 
-    /** 权重值 */
+    /**
+     * 权重值
+     */
     @Excel(name = "权重值")
     private Long weight;
 
-    /** 是否活动菜单 1代表活动页 0代表不是 只能存在一条活动首页，添加的时候要进行判定 */
+    /**
+     * 是否活动菜单 1代表活动页 0代表不是 只能存在一条活动首页，添加的时候要进行判定
+     */
     @Excel(name = "是否活动菜单 1代表活动页 0代表不是 只能存在一条活动首页，添加的时候要进行判定")
     private String activity;
 
-    public void setId(Long id) 
-    {
+    @Excel(name = "选中时菜单状态")
+    private String selectedAddr;
+
+    @Excel(name = "未选中时菜单状态")
+    private String unSelectedAddr;
+
+
+    public String getSelectedAddr() {
+        return selectedAddr;
+    }
+
+    public void setSelectedAddr(String selectedAddr) {
+        this.selectedAddr = selectedAddr;
+    }
+
+    public String getUnSelectedAddr() {
+        return unSelectedAddr;
+    }
+
+    public void setUnSelectedAddr(String unSelectedAddr) {
+        this.unSelectedAddr = unSelectedAddr;
+    }
+
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public Long getId() 
-    {
+    public Long getId() {
         return id;
     }
-    public void setMenuName(String menuName) 
-    {
+
+    public void setMenuName(String menuName) {
         this.menuName = menuName;
     }
 
-    public String getMenuName() 
-    {
+    public String getMenuName() {
         return menuName;
     }
-    public void setWeight(Long weight) 
-    {
+
+    public void setWeight(Long weight) {
         this.weight = weight;
     }
 
-    public Long getWeight() 
-    {
+    public Long getWeight() {
         return weight;
     }
-    public void setActivity(String activity) 
-    {
+
+    public void setActivity(String activity) {
         this.activity = activity;
     }
 
-    public String getActivity() 
-    {
+    public String getActivity() {
         return activity;
     }
 
     @Override
     public String toString() {
-        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-            .append("id", getId())
-            .append("menuName", getMenuName())
-            .append("weight", getWeight())
-            .append("activity", getActivity())
-            .toString();
+        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+                .append("id", getId())
+                .append("menuName", getMenuName())
+                .append("weight", getWeight())
+                .append("activity", getActivity())
+                .toString();
     }
 }
