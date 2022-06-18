22ff54d317af8e6fad429ba822654b35a781324a
xiaoxingkong <xingxing@qq.com>
2022-06-18 19:35:17 +0700

    判断转账审核

--- hash-audit/src/main/java/com/ruoyi/audit/controller/WalletWithdrawOrderController.java
+++ hash-audit/src/main/java/com/ruoyi/audit/controller/WalletWithdrawOrderController.java
@@ -1,9 +1,18 @@
 package com.ruoyi.audit.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.servlet.http.HttpServletResponse;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.ruoyi.common.constant.Global;
 import com.ruoyi.common.constant.HttpStatus;
+import com.ruoyi.common.utils.http.HttpUtils;
+import com.ruoyi.common.utils.sign.Md5Utils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,6 +41,10 @@ import com.ruoyi.common.core.page.TableDataInfo;
 @RestController
 @RequestMapping("/hash-audit/auditWidthdrawOrder")
 public class WalletWithdrawOrderController extends BaseController {
+
+    @Value("${service.wallet-addr}")
+    public String serviceWalletAddr;
+
     @Autowired
     private IWalletWithdrawOrderService walletWithdrawOrderService;
 
@@ -85,13 +98,46 @@ public class WalletWithdrawOrderController extends BaseController {
     @Log(title = "提现审核", businessType = BusinessType.UPDATE)
     @PutMapping
     public AjaxResult edit(@RequestBody WalletWithdrawOrder walletWithdrawOrder) {
-        walletWithdrawOrder.setCheckPerson(getUsername());
         walletWithdrawOrder.setUpdateBy(getUsername());
-        if (walletWithdrawOrderService.updateWalletWithdrawOrder(walletWithdrawOrder) > 0) {
-            return new AjaxResult(HttpStatus.SUCCESS, "领取成功");
+
+        final WalletWithdrawOrder order = walletWithdrawOrderService.selectWalletWithdrawOrderById(walletWithdrawOrder.getId());
+        if (order.getCheckPerson() != null) {
+            if (!order.getCheckPerson().equals(getUsername())) {
+                new AjaxResult(HttpStatus.ERROR, "该订单无权限操作");
+            }
         } else {
-            return new AjaxResult(HttpStatus.ERROR, "当前订单已被领取，不可再次操作");
+            walletWithdrawOrder.setCheckPerson(getUsername());
         }
+
+        //判断状态是否等于3
+        if (walletWithdrawOrder.getCheckStatus() == 3) {
+
+            if (walletWithdrawOrder.getStatus() == 0) {
+                Map<String, Object> param = new HashMap<>();
+                param.put("orderId", walletWithdrawOrder.getId());
+
+                final String hash = Md5Utils.hash(Global.USER_SECRET_KEY + walletWithdrawOrder.getId());
+                param.put("sign", hash);
+
+                final String s = HttpUtils.sendPost(serviceWalletAddr + "/wallet/back/withdrawPass", param);
+                if (StringUtils.isBlank(s)) {
+                    return AjaxResult.error();
+                }
+                final JSONObject jsonObject = JSON.parseObject(s);
+
+                if (!jsonObject.getString("code").equals("200")) {
+                    return AjaxResult.error(jsonObject.getString("msg"));
+                }
+
+                walletWithdrawOrderService.updateWalletWithdrawOrder(walletWithdrawOrder);
+                return AjaxResult.success("审核通过，已转账");
+            }
+
+
+        }
+
+
+        return toAjax(walletWithdrawOrderService.updateWalletWithdrawOrder(walletWithdrawOrder) > 0);
     }
 
     /**
