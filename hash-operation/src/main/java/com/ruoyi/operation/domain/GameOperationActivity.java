5b172e8fe53d677c92a2c889869951c94ae952d2
huilang <yangyang1040553@gmail.com>
2022-06-28 13:28:52 +0700

    修改 活动 增加活动内容字段

--- hash-operation/src/main/java/com/ruoyi/operation/domain/GameOperationActivity.java
+++ hash-operation/src/main/java/com/ruoyi/operation/domain/GameOperationActivity.java
@@ -7,62 +7,73 @@ import com.ruoyi.common.core.domain.BaseEntity;
 
 /**
  * 运营活动对象 t_game_operation_activity
- * 
+ *
  * @author xxk
  * @date 2022-06-26
  */
-public class GameOperationActivity extends BaseEntity
-{
+public class GameOperationActivity extends BaseEntity {
     private static final long serialVersionUID = 1L;
 
-    /** 1:周累计 2救援金 3每日活动 4连赢在赢 5新人包赢 */
+    /**
+     * 1:周累计 2救援金 3每日活动 4连赢在赢 5新人包赢
+     */
     @Excel(name = "1:周累计 2救援金 3每日活动 4连赢在赢 5新人包赢")
     private Long id;
 
-    /** 配置数据 */
+    /**
+     * 配置数据
+     */
     @Excel(name = "配置数据")
     private String json;
 
-    /** 是否开启 */
+    /**
+     * 是否开启
+     */
     @Excel(name = "是否开启")
     private Long isOpen;
 
-    public void setId(Long id) 
-    {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public Long getId() 
-    {
+    public Long getId() {
         return id;
     }
-    public void setJson(String json) 
-    {
+
+    public void setJson(String json) {
         this.json = json;
     }
 
-    public String getJson() 
-    {
+    public String getJson() {
         return json;
     }
-    public void setIsOpen(Long isOpen) 
-    {
+
+    public void setIsOpen(Long isOpen) {
         this.isOpen = isOpen;
     }
 
-    public Long getIsOpen() 
-    {
+    public Long getIsOpen() {
         return isOpen;
     }
 
     @Override
     public String toString() {
-        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-            .append("id", getId())
-            .append("json", getJson())
-            .append("isOpen", getIsOpen())
-            .append("createTime", getCreateTime())
-            .append("updateTime", getUpdateTime())
-            .toString();
+        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+                .append("id", getId())
+                .append("json", getJson())
+                .append("isOpen", getIsOpen())
+                .append("createTime", getCreateTime())
+                .append("updateTime", getUpdateTime())
+                .toString();
     }
 }
