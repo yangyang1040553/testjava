3c0a65b646c1f0dfbe6d38054e7bb60a83dfb792
huilang <yangyang1040553@gmail.com>
2022-07-13 15:38:56 +0700

    优化

--- hash-wallet/src/main/java/com/ruoyi/wallet/domain/WalletTurnover.java
+++ hash-wallet/src/main/java/com/ruoyi/wallet/domain/WalletTurnover.java
@@ -72,6 +72,17 @@ public class WalletTurnover extends BaseEntity {
     @Excel(name = "昵称")
     private String nickName;
 
+    @Excel(name = "备注")
+    private String note_name;
+
+    public String getNote_name() {
+        return note_name;
+    }
+
+    public void setNote_name(String note_name) {
+        this.note_name = note_name;
+    }
+
     public String getNickName() {
         return nickName;
     }
