72bc660fe8eb7f43378e2cfc5f1e21dc75fd7b4e
xiaoxingkong <xiaoxingkong1078@gmail.com>
2022-09-02 18:33:51 +0700

    增加key

--- hash-system/src/main/java/com/ruoyi/system/service/Test.java
+++ hash-system/src/main/java/com/ruoyi/system/service/Test.java
@@ -9,11 +9,11 @@ import java.net.UnknownHostException;
 public class Test {
 
     public static void main(String[] args) {
-        String s = SecurityUtils.encryptPassword("$Xy123456!");
+        String s = SecurityUtils.encryptPassword("hash@#2022....");
         System.out.println(s);
-        String secret = GoogleAuthenticator.genSecret("admin");
+        String secret = GoogleAuthenticator.genSecret("测试服账号");
         System.out.println(secret);
-        String qrCode = GoogleAuthenticator.getQRBarcodeURL("admin","Hash", secret);
+        String qrCode = GoogleAuthenticator.getQRBarcodeURL("测试服账号","Hash", secret);
         System.out.println(qrCode);
 
         boolean authcode = GoogleAuthenticator.authcode("642607", secret);
